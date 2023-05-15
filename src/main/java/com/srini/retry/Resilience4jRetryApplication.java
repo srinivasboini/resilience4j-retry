c92505aaf5891aa21ebdacd21fb47e444340d3cf
Srini <srinivasboini7@gmail.com>
2023-05-15 14:53:41 +0800

    Initial commit

---
+++ src/main/java/com/srini/retry/Resilience4jRetryApplication.java
@@ -0,0 +1,20 @@
+package com.srini.retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Resilience4jRetryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jRetryApplication.class, args);
+	}
+
+
+	public RestTemplate restTemplate(){
+		return new RestTemplateBuilder().build() ;
+	}
+
+}
---
+++ src/test/java/com/srini/retry/Resilience4jRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srini.retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jRetryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
